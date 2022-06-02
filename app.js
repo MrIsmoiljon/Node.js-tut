9e1946c91b74fb3506cef290d56bcba782324dc7
MrIsmoiljon <abduraimovismoil98@gmail.com>
2022-06-02 15:50:05 +0900

    Event Emitter

--- app.js
+++ app.js
@@ -1,36 +1,11 @@
-const { readFile, writeFile } = require("fs").promises;
-// const util = require("util");
-// const readFilePromise = util.promisify(readFile);
-// const writeFilePromise = util.promisify(writeFile);
+const http = require("http");
 
-const start = async () => {
-  try {
-    const first = await readFile("./content/first.txt", "utf8");
-    const second = await readFile("./content/second.txt", "utf8");
-    await writeFile(
-      "./content/result-mind-grenade.txt",
-      `this is awesome: ${first} ${second}`
-    );
-    console.log(first, second);
-  } catch (error) {
-    console.log(error);
-  }
-};
+// Using Event Emitter API
+const server = http.createServer();
+// emit request event
+// subscribe to it / listen for it / respond to it
+server.on("request", (req, res) => {
+  res.end("Welcome");
+});
 
-start();
-
-// const getText = (path) => {
-//   return new Promise((resolve, reject) => {
-//     readFile(path, "utf8", (err, data) => {
-//       if (err) {
-//         reject(err);
-//       } else {
-//         resolve(data);
-//       }
-//     });
-//   });
-// };
-
-// getText("./content/first.txt")
-//   .then((result) => console.log(result))
-//   .catch((err) => console.log(err));
+server.listen(3000);
