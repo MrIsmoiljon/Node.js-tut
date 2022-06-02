506fab987bb3c4ee80a09f2cd5a1fe91d22452af
MrIsmoiljon <abduraimovismoil98@gmail.com>
2022-06-02 15:26:31 +0900

    added file

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
-const { readFile } = require("fs");
-
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
-
-// getText("./content/first.txt")
-//   .then((result) => console.log(result))
-//   .catch((err) => console.log(err));
+const { readFile, writeFile } = require("fs").promises;
+// const util = require("util");
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
 const start = async () => {
   try {
-    const first = await getText("./content/first.txt");
-    const second = await getText("./content/second.txt");
+    const first = await readFile("./content/first.txt", "utf8");
+    const second = await readFile("./content/second.txt", "utf8");
+    await writeFile(
+      "./content/result-mind-grenade.txt",
+      `this is awesome: ${first} ${second}`
+    );
     console.log(first, second);
   } catch (error) {
     console.log(error);
@@ -27,3 +18,19 @@ const start = async () => {
 };
 
 start();
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+
+// getText("./content/first.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
